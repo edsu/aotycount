11a54b3ca42961d9c635068d1409fee9decacec2
Ed Summers <ehs@pobox.com>
2023-12-11 11:12:50 -0500

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# aotycount
+
+Count common albums for the year across lists at https://aoty.hubmed.org/.
---
+++ aotycount.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main(year: int) -> None:
+    item_count = defaultdict(list)
+    for list_url in aoty_lists(year):
+        for item, list_title in aoty_list(list_url):
+            item_count[item].append(list_title)
+    for name in sorted(item_count.keys(), reverse=True, key=lambda k: len(item_count[k])):
+        lists = item_count[name]
+        print(f"[{len(lists)}] {name} [{', '.join(lists)}]")
+
+def aoty_lists(year: str) -> list[str]:
+    url = 'https://aoty.hubmed.org/'
+    soup = _get(url)
+    for el in soup.select('.item .meta a'):
+        if el.text.startswith(year):
+            yield urljoin(url, el.attrs['href'])
+
+def aoty_list(url: str) -> list[str]:
+    soup = _get(url)
+    title = soup.select_one('a.title').text
+    for el in soup.select('.albumlist li'):
+        [artist, album] = el.select("span[itemprop='name']")
+        yield f"{artist.text} - {album.text}", title
+
+def _get(url: str) -> requests.Response:
+    html = requests.get(url).text
+    return BeautifulSoup(html, 'html.parser')
+
+if __name__ == "__main__":
+    year = sys.argv[1]
+    main(year)
